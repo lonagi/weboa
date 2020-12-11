9ec11071c08cb15675e62da64e187249c9cf711d
Sasha <lonagi22@gmail.com>
2020-12-11 22:46:30 +0200

    Create folders and files

--- create.py
+++ create.py
@@ -1,6 +1,20 @@
 import sys, os
+
 class General:
     BUILDFOLDER = "build"
 
     def __init__(self):
         self.path = "./"
+
+    def Folder_Create(self,foldername):
+        try:
+            os.mkdir(os.path.join(self.path,foldername))
+            return True
+        except FileExistsError:
+            print(f"Folder {foldername} exists.")
+            return False
+
+    def File_Create(self,filename,text=""):
+        # Creating a file at specified location
+        with open(os.path.join(self.path, filename), 'w') as f:
+            f.write(text)
