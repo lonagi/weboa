fe7658db39ba5fb6fdd0134caa6632bccaaa0340
Sasha <lonagi22@gmail.com>
2020-12-12 16:45:57 +0200

    Update myMail.php

--- res/phplib/mail/myMail.php
+++ res/phplib/mail/myMail.php
@@ -1,46 +1,34 @@
 <?php
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
+ 
 require 'src/Exception.php';
 require 'src/PHPMailer.php';
 require 'src/SMTP.php';
 
 function sendM($to,$s,$m, $f = 0, $h = false)
 {
-//	$passs = "1234123235123423532112341232351234235321";
-    $passs = "Agricos25042020!"; //google account password
-//	$passs = "rywtxgroxqqgxrgb!"; //google application password
-//	$passs = "***"; //yandex
+    $passs = "";
 
-//	$from = 'no-reply@agricole.md';
-    $from = 'agricos.md@gmail.com';
-//	$from = 'agricole.md@yandex.ru';
+    $from = 'no-reply@nvg-group.com';
     $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    $mail->SMTPDebug = 0;
-//	$mail->Host = "smtp.agricole.md";
-    $mail->Host = "smtp.gmail.com";
-//	$mail->Host = "smtp.yandex.ru";
-//	$mail->Port = "25"; // typically 587
-    $mail->Port = 465;
+    $mail->isSMTP(); 
+    $mail->SMTPDebug = 0; 
+    $mail->Host = "smtp.yandex.ru"; 
+    $mail->Port = "465"; // typically 587 
     $mail->SMTPSecure = 'ssl'; // ssl is depracated
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->SMTPAuth = true;
     $mail->Username = $from;
     $mail->CharSet = "utf-8";
     $mail->Password = $passs;
     $mail->addReplyTo($from);
-    $mail->setFrom($from, "Agricole");
+    $mail->setFrom($from, "NVG Group");
     $mail->addAddress($to);
     $mail->Subject = $s;
 
-
-
     if($h)
     {
-        $mail->MsgHTML($m); // remove if you do not want to send HTML email
+        $mail->msgHTML($m); // remove if you do not want to send HTML email
         $mail->AltBody = 'HTML not supported';
     }
     else
@@ -53,5 +41,4 @@ function sendM($to,$s,$m, $f = 0, $h = false)
         $mail->AddAttachment($f['tmp_name'], $f['name']);
     $mail->send();
 }
-
 ?>
