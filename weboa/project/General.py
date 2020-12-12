9713f760744136a9d4ed54abc919a1f1ce39f56d
Sasha <lonagi22@gmail.com>
2020-12-13 00:25:26 +0200

    UmbrellaJS

--- weboa/project/General.py
+++ weboa/project/General.py
@@ -3,6 +3,24 @@ from weboa.utils import Processing
 
 import os
 
+class UmbrellaJS:
+    def __init__(self):
+        self.name = "Umbrella JS"
+        self.url = "https://cdn.jsdelivr.net/npm/umbrellajs"
+    def load(self):
+        return self.url
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __add__(self, other):
+        return other+self.__str__()
+
+    def __radd__(self, other):
+        return other+self.__str__()
 class General(Processing):
     def __init__(self, langs=("en","ru"), version="0"):
         super().__init__()
