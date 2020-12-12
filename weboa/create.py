e2051ed3484d2fb1af1fe37f1e3ea8544348e53d
Sasha <lonagi22@gmail.com>
2020-12-12 17:04:54 +0200

    fraction of processing

--- weboa/create.py
+++ weboa/create.py
@@ -1,49 +1,9 @@
 from PIL import Image
-from shutil import copy2
-from shutil import copytree as copytree2
 from weboa.utils import *
 
-import sys, os, io
+import os
 
-class Proccessing:
-    BUILDFOLDER = "build"
-
-    def __init__(self):
-        self.path = "../"
-        self.os = sys.platform
-        #Printer.info("Platform",sys.platform)
-        if(self.os in ["Windows","win32","win64","win"]):
-            self.os = "Windows"
-
-    def Folder_Create(self, foldername):
-        try:
-            os.mkdir(os.path.join(self.path, foldername))
-            return True
-        except FileExistsError:
-            Printer.warning(f"Folder {foldername} exists.")
-            return False
-
-    def File_Create(self, filename, text=""):
-        # Creating a file at specified location
-        with io.open(os.path.join(self.path, filename), 'w', encoding="utf-8") as f:
-            f.write(text)
-
-    def copy(self, src, dst):
-        copy2(self.path + src, os.path.join(self.path, self.BUILDFOLDER) + dst)
-
-    def copytree(self, src, dst):
-        try:
-            copytree2(self.path + src, os.path.join(self.path, self.BUILDFOLDER) + dst)
-        except FileExistsError:
-            pass
-
-    def Trime(self, text):
-        return text.replace("\t", "").replace("  ", "")
-
-    def Delete_Lines(self, text):
-        return text.replace("\n", "")
-
-class General(Proccessing):
+class General(Processing):
     def __init__(self, langs=("en","ru")):
         super().__init__()
         self.langs = langs
--- weboa/utils/__init__.py
+++ weboa/utils/__init__.py
@@ -1,2 +1,3 @@
 from .Console_Color import *
 from .Printer import *
+from .Processing import *
