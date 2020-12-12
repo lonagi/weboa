f86dd6645b0f6935693381a6a3c233243fb9f5b8
Sasha <lonagi22@gmail.com>
2020-12-12 17:28:35 +0200

    refactor php-py

--- weboa/create.py
+++ weboa/create.py
@@ -1,43 +1,7 @@
-from PIL import Image
 from weboa.utils import *
+from weboa.project import *
 
 import os
-
-class General(Processing):
-    def __init__(self, langs=("en","ru")):
-        super().__init__()
-        self.langs = langs
-
-    def robots(self):
-        self.copy('res/misc/robots.txt',"/robots.txt")
-
-    def ico(self):
-        img = Image.new('RGB', (64, 64))
-        icon_sizes = [(16, 16), (32, 32), (48, 48), (64, 64)]
-        img.save(os.path.join(self.path, self.BUILDFOLDER)+'/favicon.ico', sizes=icon_sizes)
-
-    def css(self):
-        files = ("/css/styles.css", "/css/styles.min.css")
-        for f in files:
-            self.File_Create(self.BUILDFOLDER + f)
-
-    def js(self):
-        files = ("/js/script.js","/js/script.min.js")
-        for f in files:
-            self.File_Create(self.BUILDFOLDER + f)
-
-    def img(self):
-        img = Image.new('RGB', (128, 128))
-        img.save(os.path.join(self.path, self.BUILDFOLDER) + '/img/favicon.png')
-        img = Image.new('RGB', (1024, 500))
-        img.save(os.path.join(self.path, self.BUILDFOLDER) + '/img/sn_share.png')
-
-    def readme(self):
-        self.copy('res/misc/README.md',"/README.md")
-
-    def ico_langs(self):
-        for l in self.langs:
-            self.copy('res/ico_langs/'+l+'.svg',"/img/"+l+".svg")
 
 class PHP(General):
     def __init__(self, langs=("en","ru")):
@@ -56,9 +20,9 @@ class PHP(General):
 
     def language(self):
         # Language system
-        self.copy('res/phpfs/language.php',"/php/controller/language.php")
+        self.copy('res/phpfs/language',"/php/controller/language.php")
         for l in self.langs:
-            self.copy('res/phpfs/l.php', f"/php/configs/{l}.php")
+            self.copy('res/phpfs/l', f"/php/configs/{l}.php")
 
     def controller(self):
         files = ("controller.php","index.php","router.php")
@@ -69,9 +33,9 @@ class PHP(General):
         self.copy('res/phpfs/.htaccess',"/.htaccess")
 
     def project(self):
-        self.copy('res/phpfs/db.php',"/php/db.php")                     # DATABASE
-        self.copy('res/phpfs/test.php',"/php/api/test.php")             # API
-        self.copy('res/phpfs/consts.php',"/php/configs/consts.php")     # Consts
+        self.copy('res/phpfs/db',"/php/db.php")                     # DATABASE
+        self.copy('res/phpfs/test',"/php/api/test.php")             # API
+        self.copy('res/phpfs/consts',"/php/configs/consts.php")     # Consts
 
     def libs(self):
         self.copy('res/phpfs/autoload.php', "/php/lib/autoload.php")
---
+++ weboa/project/General.py
@@ -0,0 +1,40 @@
+from PIL import Image
+from weboa.utils import Processing
+
+import os
+
+class General(Processing):
+    def __init__(self, langs=("en","ru")):
+        super().__init__()
+        self.langs = langs
+
+    def robots(self):
+        self.copy('res/misc/robots.txt',"/robots.txt")
+
+    def ico(self):
+        img = Image.new('RGB', (64, 64))
+        icon_sizes = [(16, 16), (32, 32), (48, 48), (64, 64)]
+        img.save(os.path.join(self.path, self.BUILDFOLDER)+'/favicon.ico', sizes=icon_sizes)
+
+    def css(self):
+        files = ("/css/styles.css", "/css/styles.min.css")
+        for f in files:
+            self.File_Create(self.BUILDFOLDER + f)
+
+    def js(self):
+        files = ("/js/script.js","/js/script.min.js")
+        for f in files:
+            self.File_Create(self.BUILDFOLDER + f)
+
+    def img(self):
+        img = Image.new('RGB', (128, 128))
+        img.save(os.path.join(self.path, self.BUILDFOLDER) + '/img/favicon.png')
+        img = Image.new('RGB', (1024, 500))
+        img.save(os.path.join(self.path, self.BUILDFOLDER) + '/img/sn_share.png')
+
+    def readme(self):
+        self.copy('res/misc/README.md',"/README.md")
+
+    def ico_langs(self):
+        for l in self.langs:
+            self.copy('res/ico_langs/'+l+'.svg',"/img/"+l+".svg")
