3f2f674b4f5cdb37fd8e14924cf894e82374a358
Sasha <lonagi22@gmail.com>
2020-12-12 17:38:31 +0200

    lang import fix

--- res/phpfs/controller.php
+++ res/phpfs/controller.php
@@ -2,6 +2,8 @@
 include_once "language.php";
 if(in_array($lang, $langs))
     include_once("php/configs/".$lang.".php");
+else
+    include_once("php/configs/en.php");
 
 include_once "php/configs/consts.php";
 
