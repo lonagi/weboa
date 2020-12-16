b71d3b4cb50a6a822633710e68dad5d8c82b6d7f
Sasha <lonagi22@gmail.com>
2020-12-16 23:51:44 +0200

    test creation of php project

--- weboa/run.py
+++ weboa/run.py
@@ -6,12 +6,20 @@ import sys
 def runcli():
     print("Welcome to Weboa!")
     commands = {
-        "version": ("--version", "-v")
+        "version": ("--version", "-v"),
+        "init": ("--init","-i"),
     }
 
     for arg in sys.argv:
         if arg in commands["version"]:
             print(f"Weboa version is {__VERSION__}")
+        elif arg in commands["init"]:
+            php=PHP()
+            php.FS()
+            php.index()
+            php.language()
+            php.ico()
+            php.css()
 
 if(__name__=="__main__"):
     runcli()
