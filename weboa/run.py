9f52ea0e6db7dd35bae9f136eaf46cfce70f5af1
Sasha <lonagi22@gmail.com>
2020-12-19 00:07:41 +0200

    less command

--- weboa/run.py
+++ weboa/run.py
@@ -2,32 +2,62 @@
 from weboa import *
 from weboa import __VERSION__
 import sys
+import glob
+import lesscpy
+from six import StringIO
 
 def runcli():
     print("Welcome to Weboa!")
     commands = {
         "version": ("--version", "-v"),
         "init": ("--init","-i"),
-        "langs": ("--langs", "-l"),
+        "langs": ("--langs", "-L"),
         "start": ("--start", "-s"),
-        "update": ("--update","-u")
+        "update": ("--update","-u"),
+        "less": ("--less","-l")
     }
 
     args = sys.argv
     for i in range(len(args)):
         if args[i] in commands["version"]:
             print(f"Weboa version is {__VERSION__}")
+
         elif args[i] in commands["update"]:
             os.system("pip install weboa --upgrade")
             os.system("pip3 install weboa --upgrade")
+
         elif args[i] in commands["start"]:
             Processing.Save_Path(os.getcwd())
             Printer.info(f"Weboa is installed at {prepare.Package.stream}")
 
+        elif args[i] in commands["less"]:
+            _path = os.getcwd()
+            _weboa = Processing.Weboa_Open()
+
+            while True:
+                for i in glob.glob(_path+"/*.less"):
+                    if "less" not in _weboa.keys():
+                        _weboa["less"] = dict()
+                    if i not in _weboa["less"].keys():
+                        _weboa["less"][i] = 0
+
+                    ts0 = _weboa["less"][i]
+                    ts1 = os.stat(i).st_mtime
+
+                    if(ts0==ts1):
+                        continue
+
+                    with open(i,"r") as f:
+                        less = f.read()
+                        css = lesscpy.compile(StringIO(less), minify=True)
+                    with open(i[:-4]+"css","w") as f:
+                        f.write(css)
+                    _weboa["less"][i] = os.stat(i).st_mtime
+                    Processing.Weboa_Save(_weboa)
+
         elif args[i] in commands["init"]:
             _path = os.getcwd()
             Processing.Save_Path(_path)
-
             try:
                 if commands["langs"][0] in args:
                     lindex = args.index(commands["langs"][0])
@@ -37,7 +67,6 @@ def runcli():
                 Printer.info(f"Langs {args[lindex+1]}")
             except IndexError:
               pass
-
             php=PHP(path="")
             php.BUILDFOLDER = _path+"/"
             php.FS()
