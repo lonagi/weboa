e900b74cf72e400b40576720efb5a7f04b742923
Sasha <lonagi22@gmail.com>
2021-01-06 00:11:30 +0200

    update mdb5 to 3.0.0

--- weboa/libraries/MDB5.py
+++ weboa/libraries/MDB5.py
@@ -3,5 +3,5 @@ from weboa.libraries import Library
 class MDB5(Library):
     def __init__(self):
         self.name = "Bootstrap 5 Material 2"
-        self.js = ["https://ex.nvg-group.com/libs/mdb/2.2.1/mdb.min.js"]
-        self.css = ["https://ex.nvg-group.com/libs/mdb/2.2.1/mdb.min.css"]
+        self.js = ["https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.0.0/mdb.min.js"]
+        self.css = ["https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.0.0/mdb.min.css"]
